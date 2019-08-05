bcea8ebe45b0e808f291dd88c3f71fd757bc12ca
Chris Korbel <ckorbel@Chriss-MacBook-Pro.local>
2019-08-05 08:30:17 -0700

    modal change

--- models/User.js
+++ models/User.js
@@ -1,21 +1,38 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   firstName: {
     type: String,
-    required: true
+    required: true,
+    trim: true,
+    maxlength: 35
   },
   lastName: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 35
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    maxlength: 45,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("email is invalid");
+      }
+    }
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 6,
+    trim: true
   },
   date: {
     type: Date,
